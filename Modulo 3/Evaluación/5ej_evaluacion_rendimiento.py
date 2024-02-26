6b2d302e407a958628d756846736177e86e76a45
Aldo Carquín <aldo.gonzalez.carquin@gmail.com>
2024-02-26 04:14:09 -0300

    modificaciones

--- Modulo 3/Evaluación/5ej_evaluacion_rendimiento.py
+++ Modulo 3/Evaluación/5ej_evaluacion_rendimiento.py
@@ -1,7 +1,8 @@
 import math
 
+materias = ['Lenguaje', 'Matemáticas', 'Historia', 'Ciencias', 'Música', 'Arte']
 lenguaje = [3, 4, 5, 6]
-matematica = [5, 6, 3, 5]
+matematicas = [5, 6, 3, 5]
 historia = [2]
 ciencias = [2, 2, 2]
 musica = [4, 5, 2]
@@ -20,40 +21,27 @@ def hayNota(asignatura):
                 print(asignatura[i], end='\n')
 
 
-def ver(lenguaje, matematica, historia, ciencias, musica, arte, asignatura):
+def ver(asignatura, materias):
     print(f'\n*** LAS NOTAS SON LAS SIGUIENTES ***')
-    
-    # LENGUAJE
-    asignatura = list(lenguaje)
-    prom = promedio(asignatura)
-    print(f'LENGUAJE: Promedio {prom}')
-    hayNota(asignatura)
 
-    asignatura = list(matematica)
-    prom = promedio(asignatura)
-    print(f'\n\nMATEMÁTICAS: Promedio {prom}')
-    hayNota(asignatura)
-
-    asignatura = list(historia)
-    prom = promedio(asignatura)
-    print('\n\nHISTORIA: Promedio {prom}')
-    hayNota(asignatura)
-
-    asignatura = list(ciencias)
-    prom = promedio(asignatura)
-    print('\n\nCIENCIAS: Promedio {prom}')
-    hayNota(asignatura)
-
-    asignatura = list(musica)
-    prom = promedio(asignatura)
-    print(f'\n\nMÚSICA: Promedio {prom}')
-    hayNota(asignatura)
-
-    asignatura = list(arte)
-    prom = promedio(asignatura)
-    print(f'\n\nARTE: Promedio {prom}')
-    hayNota(asignatura)
-
+    for m in materias:
+        match m:
+            case 'Lenguaje':
+                asignatura = list(lenguaje)
+            case 'Matemáticas':
+                asignatura = list(matematicas)
+            case 'Historia':
+                asignatura = list(historia)
+            case 'Ciencias':
+                asignatura = list(ciencias)
+            case 'Música':
+                asignatura = list(musica)
+            case 'Arte':
+                asignatura = list(arte)
+            
+        prom = promedio(asignatura)
+        print(f'\n{m}: Promedio {prom}')
+        hayNota(asignatura)
 
 def promedio(asignatura):
     if len(asignatura) == 0:
@@ -69,13 +57,13 @@ print('2.- Agregar nota.')
 print('3.- Borrar nota.')
 print('4.- Salir')
 
-def main(lenguaje, matematica, historia, ciencias, musica, arte, asignatura):
+def main(materias, asignatura):
 
     menu = int(input('Opción:'))
     while menu != 4:
         match menu:
             case 1:
-                ver(lenguaje, matematica, historia, ciencias, musica, arte, asignatura)
+                ver(asignatura, materias)
             case 2:
                 print('Opcion 2')
             case 3:
@@ -88,7 +76,7 @@ def main(lenguaje, matematica, historia, ciencias, musica, arte, asignatura):
     
 
 if __name__ == "__main__":
-    main(lenguaje, matematica, historia, ciencias, musica, arte, asignatura)
+    main(materias, asignatura)
 
 
 
